d0388504dce70e4c7b339593f1d40f3686684b51
Rasty Espartero <gemrasty@gmail.com>
2023-10-05 23:27:50 +0800

    Create "Find the Largest Prime Factor".c

---
+++ Practice Exercises/"Find the Largest Prime Factor".c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+long long largestPrimeFactor(long long num) {
+    long long maxPrime = -1;
+    
+    while (num % 2 == 0) {
+        maxPrime = 2;
+        num /= 2;
+    }
+    
+    for (long long i = 3; i * i <= num; i += 2) {
+        while (num % i == 0) {
+            maxPrime = i;
+            num /= i;
+        }
+    }
+    
+    if (num > 2) {
+        maxPrime = num;
+    }
+    
+    return maxPrime;
+}
+
+int main() {
+    long long n;
+    
+    printf("Enter a positive integer: ");
+    scanf("%lld", &n);
+    
+    long long result = largestPrimeFactor(n);
+    
+    printf("Largest prime factor: %lld\n", result);
+    
+    return 0;
+}
