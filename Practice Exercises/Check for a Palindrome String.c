caf6d52cda04b918235cc0ca91db4278d4ff82d6
Rasty Espartero <gemrasty@gmail.com>
2023-10-07 23:39:33 +0800

    Create Check for a Palindrome String.c

---
+++ Practice Exercises/Check for a Palindrome String.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool isPalindrome(const char *str) {
+    int length = strlen(str);
+    for (int i = 0; i < length / 2; i++) {
+        if (str[i] != str[length - i - 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    char str[100];
+
+    printf("Enter a string: ");
+    gets(str);
+
+    if (isPalindrome(str)) {
+        printf("The string is a palindrome.\n");
+    } else {
+        printf("The string is not a palindrome.\n");
+    }
+
+    return 0;
+}
