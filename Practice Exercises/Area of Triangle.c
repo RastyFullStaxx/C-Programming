fb837840824297a94e4a5a6e1a0df3caa6d5fd3a
Rasty Espartero <gemrasty@gmail.com>
2023-10-10 23:56:13 +0800

    Create Area of Triangle.c

---
+++ Practice Exercises/Area of Triangle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    float base1, base2, height, area;
+
+    printf("Enter the length of the first base: ");
+    scanf("%f", &base1);
+
+    printf("Enter the length of the second base: ");
+    scanf("%f", &base2);
+
+    printf("Enter
