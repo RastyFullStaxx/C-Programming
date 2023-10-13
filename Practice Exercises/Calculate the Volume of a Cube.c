47ee0f75619a2dd680977be76fd8c197bf0610e6
Rasty Espartero <gemrasty@gmail.com>
2023-10-13 23:40:43 +0800

    Create Calculate the Volume of a Cube.c

---
+++ Practice Exercises/Calculate the Volume of a Cube.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    float side, volume;
+
+    printf("Enter the side length of the cube: ");
+    scanf("%f", &side);
+
+    volume = side * side * side;
+
+    printf("Volume of the cube: %.2f\n", volume);
+
+    return 0;
+}
