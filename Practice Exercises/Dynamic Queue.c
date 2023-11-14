dbe68a205e80d199f0517b7df2c6fc805377ae5f
Rasty Espartero <gemrasty@gmail.com>
2023-11-14 23:43:30 +0800

    Create Dynamic Queue.c

---
+++ Practice Exercises/Dynamic Queue.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Queue {
+    struct Node* front;
+    struct Node* rear;
+};
+
+void enqueue(struct Queue* queue, int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+
+    if (queue->rear == NULL) {
+        queue->front = newNode;
+        queue->rear = newNode;
+    } else {
+        queue->rear->next = newNode;
+        queue->rear = newNode;
+    }
+}
+
+int dequeue(struct Queue* queue) {
+    if (queue
