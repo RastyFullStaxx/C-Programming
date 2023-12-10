8bf9a75d21e33b7180d1fe6d71353eedc9d87c67
Rasty Espartero <gemrasty@gmail.com>
2023-12-10 23:58:15 +0800

    Create PostfixEvaluation.c

---
+++ Practice Exercises/PostfixEvaluation.c
@@ -0,0 +1,15 @@
+import java.util.Stack;
+
+public class PostfixEvaluation {
+    public static void main(String[] args) {
+        String postfixExpression = "23*5+";
+
+        int result = evaluatePostfix(postfixExpression);
+        System.out.println("Result of the postfix expression: " + result);
+    }
+
+    public static int evaluatePostfix(String expression) {
+        // Implement a function to evaluate a postfix expression using a stack
+        return 0;
+    }
+}
