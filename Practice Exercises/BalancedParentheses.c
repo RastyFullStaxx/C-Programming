bb212609693b5d382cfc95695d6a5f20364b88fd
Rasty Espartero <gemrasty@gmail.com>
2023-12-10 23:58:25 +0800

    Create BalancedParentheses.c

---
+++ Practice Exercises/BalancedParentheses.c
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+public class BalancedParentheses {
+    public static void main(String[] args) {
+        String expression1 = "{[()]}";
+        String expression2 = "{[(])}";
+
+        System.out.println("Expression 1 is balanced: " + isBalanced(expression1));
+        System.out.println("Expression 2 is balanced: " + isBalanced(expression2));
+    }
+
+    public static boolean isBalanced(String expression) {
+        // Implement a function to check if the given expression has balanced parentheses
+        return false;
+    }
+}
