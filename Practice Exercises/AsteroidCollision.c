e27b82422f3efb885d5d29403af513467ebe9cb3
Rasty Espartero <gemrasty@gmail.com>
2023-12-10 23:57:26 +0800

    Create AsteroidCollision.c

---
+++ Practice Exercises/AsteroidCollision.c
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+public class AsteroidCollision {
+    public static void main(String[] args) {
+        int[] asteroids = {5, 10, -5};
+
+        int[] result = asteroidCollision(asteroids);
+
+        System.out.println("Result after asteroid collision: " + Arrays.toString(result));
+    }
+
+    public static int[] asteroidCollision(int[] asteroids) {
+        // Implement a function to simulate asteroid collision and return the remaining asteroids
+        return new int[0];
+    }
+}
