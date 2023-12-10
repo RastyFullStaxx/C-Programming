89e94203c0c02792081eb4c3808e822e95fcf275
Rasty Espartero <gemrasty@gmail.com>
2023-12-10 23:58:06 +0800

    Create NextGreaterElement.c

---
+++ Practice Exercises/NextGreaterElement.c
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Stack;
+
+public class NextGreaterElement {
+    public static void main(String[] args) {
+        int[] nums = {4, 2, 10, 7, 5, 8};
+
+        int[] nextGreaterElements = findNextGreaterElements(nums);
+
+        System.out.println("Next Greater Elements: " + Arrays.toString(nextGreaterElements));
+    }
+
+    public static int[] findNextGreaterElements(int[] nums) {
+        // Implement a function to find the next greater element for each element in the array
+        return new int[0];
+    }
+}
