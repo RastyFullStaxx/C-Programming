9f394132ba8d61e26e8949b3d2116da1a07132cd
Rasty Espartero <gemrasty@gmail.com>
2023-12-10 23:56:34 +0800

    Create SimplifyDirectoryPath.c

---
+++ Practice Exercises/SimplifyDirectoryPath.c
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+public class SimplifyDirectoryPath {
+    public static void main(String[] args) {
+        String path = "/home/user/.././documents";
+
+        String simplifiedPath = simplifyPath(path);
+
+        System.out.println("Simplified Directory Path: " + simplifiedPath);
+    }
+
+    public static String simplifyPath(String path) {
+        // Implement a function to simplify a given directory path
+        return "";
+    }
+}
