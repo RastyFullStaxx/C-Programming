1b527e8fc8d2a27a5c0644b4cea1063af5b4c9b6
Rasty Espartero <gemrasty@gmail.com>
2023-12-10 23:57:48 +0800

    Create LargestRectangleInHistogram.c

---
+++ Practice Exercises/LargestRectangleInHistogram.c
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+public class LargestRectangleInHistogram {
+    public static void main(String[] args) {
+        int[] heights = {2, 1, 5, 6, 2, 3};
+
+        int largestRectangleArea = calculateLargestRectangleArea(heights);
+
+        System.out.println("Largest Rectangle Area: " + largestRectangleArea);
+    }
+
+    public static int calculateLargestRectangleArea(int[] heights) {
+        // Implement a function to calculate the largest rectangle area in a histogram
+        return 0;
+    }
+}
