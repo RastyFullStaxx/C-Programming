dd823934c696beeb57c6975040666f724c51bd23
Rasty Espartero <gemrasty@gmail.com>
2023-12-25 23:10:06 +0800

    Create BasicCalculatorII.java

---
+++ Practice Exercises/BasicCalculatorII.java
@@ -0,0 +1,14 @@
+public class BasicCalculatorII {
+    public static void main(String[] args) {
+        String s = "3+2*2";
+
+        System.out.println("String s: " + s);
+        int result = calculate(s);
+        System.out.println("Result of Basic Calculator II: " + result);
+    }
+
+    public static int calculate(String s) {
+        // Implement a method to evaluate a simple arithmetic expression represented as a string
+        return 0;
+    }
+}
