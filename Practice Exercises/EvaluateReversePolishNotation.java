b9a5f113ed8722a5257bffe48ca4978fe0e925fc
Rasty Espartero <gemrasty@gmail.com>
2023-12-25 23:11:49 +0800

    Create EvaluateReversePolishNotation.java

---
+++ Practice Exercises/EvaluateReversePolishNotation.java
@@ -0,0 +1,16 @@
+public class LongestWordInDictionary {
+    public static void main(String[] args) {
+        String s = "abpclea";
+        List<String> dictionary = List.of("ale", "apple", "monkey", "plea");
+
+        System.out.println("String s: " + s);
+        System.out.println("Dictionary: " + dictionary);
+        String longestWord = findLongestWord(s, dictionary);
+        System.out.println("Longest Word in Dictionary through Deleting: " + longestWord);
+    }
+
+    public static String findLongestWord(String s, List<String> dictionary) {
+        // Implement a method to find the longest word in the dictionary that can be formed by deleting some characters of the given string
+        return null;
+    }
+}
