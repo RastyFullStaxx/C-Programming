5e414f11115b090ef9b22ab9811ea4a2c9df5121
Rasty Espartero <gemrasty@gmail.com>
2023-12-25 23:10:28 +0800

    Create RepeatedSubstringPattern.java

---
+++ Practice Exercises/RepeatedSubstringPattern.java
@@ -0,0 +1,14 @@
+public class RepeatedSubstringPattern {
+    public static void main(String[] args) {
+        String s = "abab";
+
+        System.out.println("String s: " + s);
+        boolean isRepeatedPattern = repeatedSubstringPattern(s);
+        System.out.println("Is Repeated Substring Pattern: " + isRepeatedPattern);
+    }
+
+    public static boolean repeatedSubstringPattern(String s) {
+        // Implement a method to check if a string can be formed by repeating a substring
+        return false;
+    }
+}
