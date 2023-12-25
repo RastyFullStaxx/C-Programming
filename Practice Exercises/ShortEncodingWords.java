85fcfe97898e6851723bd17672863a091a8b940a
Rasty Espartero <gemrasty@gmail.com>
2023-12-25 23:11:29 +0800

    Create ShortEncodingWords.java

---
+++ Practice Exercises/ShortEncodingWords.java
@@ -0,0 +1,14 @@
+public class ShortEncodingWords {
+    public static void main(String[] args) {
+        String[] words = {"time", "me", "bell"};
+
+        System.out.println("Array of Words: " + Arrays.toString(words));
+        int minLengthEncoding = minimumLengthEncoding(words);
+        System.out.println("Minimum Length Encoding: " + minLengthEncoding);
+    }
+
+    public static int minimumLengthEncoding(String[] words) {
+        // Implement a method to calculate the length of the shortest encoding of words
+        return 0;
+    }
+}
