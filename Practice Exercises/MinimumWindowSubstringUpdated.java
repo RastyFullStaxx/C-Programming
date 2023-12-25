f593634cace95cfc907c3489eff1c2b2e90cb804
Rasty Espartero <gemrasty@gmail.com>
2023-12-25 23:08:52 +0800

    Create MinimumWindowSubstringUpdated.java

---
+++ Practice Exercises/MinimumWindowSubstringUpdated.java
@@ -0,0 +1,16 @@
+public class MinimumWindowSubstringUpdated {
+    public static void main(String[] args) {
+        String s = "ADOBECODEBANC";
+        String t = "ABC";
+
+        System.out.println("String s: " + s);
+        System.out.println("String t: " + t);
+        String minWindow = minWindowSubstringUpdated(s, t);
+        System.out.println("Minimum Window Substring: " + minWindow);
+    }
+
+    public static String minWindowSubstringUpdated(String s, String t) {
+        // Implement an updated method to find the minimum window in s that contains all characters of t
+        return null;
+    }
+}
