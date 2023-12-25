c3b08c333046609207de605c2ae068491f5264a6
Rasty Espartero <gemrasty@gmail.com>
2023-12-25 23:11:03 +0800

    Create LongestSubstringKRepeatingChars.java

---
+++ Practice Exercises/LongestSubstringKRepeatingChars.java
@@ -0,0 +1,16 @@
+public class LongestSubstringKRepeatingChars {
+    public static void main(String[] args) {
+        String s = "aaabb";
+        int k = 3;
+
+        System.out.println("String s: " + s);
+        System.out.println("k: " + k);
+        int longestSubstring = longestSubstring(s, k);
+        System.out.println("Longest Substring with At Least K Repeating Characters: " + longestSubstring);
+    }
+
+    public static int longestSubstring(String s, int k) {
+        // Implement a method to find the length of the longest substring with at least k repeating characters
+        return 0;
+    }
+}
